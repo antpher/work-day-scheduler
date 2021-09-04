f342e41832469728fd356d7b5484bb032d79a689
Anthony Her <antpher@gmail.com>
2021-09-04 12:45:22 -0400

    updated readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,41 @@
-// Today's date
-var todaysDate = moment().format('MMMM Do YYYY');
-$("#currentDay").html(todaysDate);
+var today = moment().format('MMMM Do YYYY');
+$("#currentDay").html(today);
 
 $(document).ready(function () {
-    // saveBtn click listener 
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Save text in local storage
         localStorage.setItem(time, text);
+    })  
+})  
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+function background () {
+    var currentHour = moment().hours();
+    $(".time-block").each(function () {
+        var divTimeId = parseInt($(this).attr("id"));
+        if (divTimeId < currentHour) {
+            $(this).addClass("past");
+        } else if (divTimeId === currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
     })
-   
-    function timeTracker() {
-        //get current number of hours.
-        var timeNow = moment().hour();
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            // To check the time and add the classes for background indicators
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-
-            }
-        })
-    }
+ }
+ 
+ background ();
